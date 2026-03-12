972ed6ccd140569e5e0ab58b89c6444afa4f7aac
pawgog <pgogola10@gmail.com>
2026-03-12 21:29:21 +0100

    Create route to room

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,7 +1,24 @@
 import { Elysia, t } from 'elysia'
+import { nanoid } from 'nanoid'
+import { redis } from '@/lib/redis'
 
-const app = new Elysia({ prefix: '/api' })
-    .get('/user', {user: {name: "John Doe", age: 30}})
+const ROOM_TTL_SECONDS = 60 * 10
+
+const rooms = new Elysia({ prefix: "/room" })
+  .post("/create", async () => {
+    const roomId = nanoid()
+
+    await redis.hset(`meta:${roomId}`, {
+      connected: [],
+      createdAt: Date.now(),
+    })
+
+    await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+
+    return { roomId }
+  })
+
+const app = new Elysia({ prefix: "/api" }).use(rooms)
 
 export const GET = app.fetch
 export const POST = app.fetch
