e5f2d1dad95b04ef571e89e2a29949081e89bf79
pawgog <pgogola10@gmail.com>
2026-03-16 18:11:48 +0100

    Create chat room page to prevent 404 error

---
+++ src/app/room/[roomId]/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Chat Room</h1>
+    </div>
+  );
+};
+
+export default Page;
