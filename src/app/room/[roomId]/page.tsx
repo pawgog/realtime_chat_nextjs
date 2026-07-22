457e7c4ab1f3b8057c61fc2107ef47c8fe239353
pawgog <pgogola10@gmail.com>
2026-07-22 18:39:03 +0200

    Add missing send message action to room page

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -91,7 +91,7 @@ const Page = () => {
                 onChange={(e) => setInput(e.target.value)}
                 onKeyDown={(e) => {
                   if (e.key === "Enter" && input.trim()) {
-                    // Send Message
+                    sendMessage({ text: input });
                     inputRef.current?.focus();
                   }
                 }}
@@ -100,7 +100,14 @@ const Page = () => {
                 className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
               />
             </div>
-            <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer">
+            <button
+              onClick={() => {
+                sendMessage({ text: input });
+                inputRef.current?.focus();
+              }}
+              disabled={!input.trim() || isPending}
+              className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+            >
               SEND
             </button>
           </div>
